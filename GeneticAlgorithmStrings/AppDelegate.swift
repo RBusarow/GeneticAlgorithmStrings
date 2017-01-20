d308cb7015ad2bd21ff6cc2352e3e7b10c7ce523
Rick Busarow <rbusarow@ricks-mbp.met.globaltti.net>
2017-01-20 09:36:42 -0600

    Initial Commit

---
+++ GeneticAlgorithmStrings/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GeneticAlgorithmStrings
+//
+//  Created by Rick Busarow on 1/20/17.
+//  Copyright © 2017 Rick Busarow. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GeneticAlgorithmStrings/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GeneticAlgorithmStrings
+//
+//  Created by Rick Busarow on 1/20/17.
+//  Copyright © 2017 Rick Busarow. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
